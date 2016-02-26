849a0563a2fcc3fd053a16d916d78a51086b0496
Igor Solovyov <igorsolovyov@me.com>
2016-02-26 14:18:04 +0100

    implemented read_u64 and read_i64;

--- src/lib.rs
+++ src/lib.rs
@@ -86,6 +86,43 @@ pub fn read_i32(data: &[u8], endianness: ByteOrder) -> Result<i32> {
     Ok( try!(read_u32(data, endianness)) as i32 )
 }
 
+pub fn read_u64(data: &[u8], endianness: ByteOrder) -> Result<u64> {
+    if data.len() < 8 {
+        Err(Error::ShortSlice)
+    } else {
+        match endianness {
+            ByteOrder::BigEndian => {
+                Ok(
+                    ( (data[0] as u64) << 56 ) +
+                    ( (data[1] as u64) << 48 ) +
+                    ( (data[2] as u64) << 40 ) +
+                    ( (data[3] as u64) << 32 ) +
+                    ( (data[4] as u64) << 24 ) +
+                    ( (data[5] as u64) << 16 ) +
+                    ( (data[6] as u64) << 8 ) +
+                    (data[7] as u64)
+                )
+            }
+            ByteOrder::LittleEndian => {
+                Ok(
+                    ( (data[7] as u64) << 56 ) +
+                    ( (data[6] as u64) << 48 ) +
+                    ( (data[5] as u64) << 40 ) +
+                    ( (data[4] as u64) << 32 ) +
+                    ( (data[3] as u64) << 24 ) +
+                    ( (data[2] as u64) << 16 ) +
+                    ( (data[1] as u64) << 8 ) +
+                    (data[0] as u64)
+                )
+            }
+        }
+    }
+}
+
+pub fn read_i64(data: &[u8], endianness: ByteOrder) -> Result<i64> {
+    Ok( try!(read_u64(data, endianness)) as i64 )
+}
+
 #[cfg(test)]
 mod tests {
     // Macro to test that all of the functions return an error type
@@ -105,18 +142,20 @@ mod tests {
                     assert_eq!(Error::ShortSlice, $read(&[], ByteOrder::LittleEndian).unwrap_err());
                 }
             }
-        )
+        );
     }
 
     short_slice!(short_u16, read_u16);
     short_slice!(short_i16, read_i16);
     short_slice!(short_u32, read_u32);
-    short_slice!(short_i32, read_u32);
+    short_slice!(short_i32, read_i32);
+    short_slice!(short_u64, read_u64);
+    short_slice!(short_i64, read_i64);
 
     // A macro to perform generative testing using the following invariant:
     // for any integer N that was transmuted to a stream of bytes read functions must return N.
     macro_rules! read_correctness {
-        ($name:ident, $ty_int:ty, $bytes: expr, $read:ident, $max:expr) => {
+        ($name:ident, $ty:ty, $size: expr, $read:ident, $max:expr) => (
             mod $name {
                 use std::mem;
                 use {ByteOrder, $read};
@@ -128,37 +167,37 @@ mod tests {
                 #[test]
                 fn read_big_endian() {
                     #[cfg(target_endian = "little")]
-                    fn prop(n: $ty_int) -> bool {
-                        let mut data = unsafe { mem::transmute::<_, [u8; $bytes]>(n as $ty_int) };
+                    fn prop(n: $ty) -> bool {
+                        let mut data = unsafe { mem::transmute::<_, [u8; $size]>(n as $ty) };
                         data.reverse();
                         n == $read(&data, ByteOrder::BigEndian).unwrap()
                     }
 
                     #[cfg(target_endian = "big")]
-                    fn prop(n: $ty_int) -> bool {
-                        let data = unsafe { mem::transmute::<_, [u8; $bytes]>(n as $ty_int) };
+                    fn prop(n: $ty) -> bool {
+                        let data = unsafe { mem::transmute::<_, [u8; $size]>(n as $ty) };
                         n == $read(&data, ByteOrder::BigEndian).unwrap()
                     }
 
-                    self::quick_check(prop as fn($ty_int) -> bool);
+                    self::quick_check(prop as fn($ty) -> bool);
                 }
 
                 #[test]
                 fn read_little_endian() {
                     #[cfg(target_endian = "little")]
-                    fn prop(n: $ty_int) -> bool {
-                        let data = unsafe { mem::transmute::<_, [u8; $bytes]>(n as $ty_int) };
+                    fn prop(n: $ty) -> bool {
+                        let data = unsafe { mem::transmute::<_, [u8; $size]>(n as $ty) };
                         n == $read(&data, ByteOrder::LittleEndian).unwrap()
                     }
 
                     #[cfg(target_endian = "big")]
-                    fn prop(n: $ty_int) -> bool {
-                        let mut data = unsafe { mem::transmute::<_, [u8; $bytes]>(n as $ty_int) };
+                    fn prop(n: $ty) -> bool {
+                        let mut data = unsafe { mem::transmute::<_, [u8; $size]>(n as $ty) };
                         data.reverse();
                         n == $read(&data, ByteOrder::LittleEndian).unwrap()
                     }
 
-                    self::quick_check(prop as fn($ty_int) -> bool);
+                    self::quick_check(prop as fn($ty) -> bool);
                 }
 
                 fn quick_check<T: Testable>(prop: T) {
@@ -167,11 +206,13 @@ mod tests {
                         .quickcheck(prop);
                 }
             }
-        }
+        );
     }
 
     read_correctness!(test_u16, u16, 2, read_u16, ::std::u16::MAX);
     read_correctness!(test_i16, i16, 2, read_i16, ::std::i16::MAX);
     read_correctness!(test_u32, u32, 4, read_u32, ::std::u32::MAX);
     read_correctness!(test_i32, i32, 4, read_i32, ::std::i32::MAX);
+    read_correctness!(test_u64, u64, 8, read_u64, ::std::u64::MAX);
+    read_correctness!(test_i64, i64, 8, read_i64, ::std::i64::MAX);
 }
